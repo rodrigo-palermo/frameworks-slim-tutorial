35f180fa107829717af95832b2825d284d5aac1f
Rodrigo Palermo <avilapalermo@gmail.com>
2019-10-05 00:40:12 -0300

    Add Doctrine, Sqlite, bootstrap.

--- index.php
+++ index.php
@@ -1,14 +1,63 @@
 <?php
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
-require './vendor/autoload.php';
-$app = new \Slim\App;
+require 'bootstrap.php';
+
 /**
- * Inicio do bang :)
- * @var string
+ * Lista de todos os livros
  */
-$app->get('/', function (Request $request, Response $response) use ($app) {
-    $response->getBody()->write("Bebê de Microservice!");
-    return $response;
+$app->get('/book', function (Request $request, Response $response) use ($app) {
+    #$response->getBody()->write("Lista de Livros");
+    #return $response;
+    $return = $response->withJson(['msg' => 'Lista de Livros'], 200);
+    return $return;
+
+});
+/**
+ * Retornando mais informações do livro informado pelo id
+ */
+$app->get('/book/{id}', function (Request $request, Response $response) use ($app) {
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');
+//    $response->getBody()->write("Exibindo o livro {$id}");
+//    return $response;
+    $return = $response->withJson(['msg' => "Exibindo o livro {$id}"], 200)
+                       ->withHeader('Content-type', 'application/json');;
+    return $return;
+});
+/**
+ * Cadastra um novo Livro
+ */
+$app->post('/book', function (Request $request, Response $response) use ($app) {
+//    $response->getBody()->write("Cadastrando um livro");
+//    return $response;
+    $return = $response->withJson(['msg' => "Cadastrando um livro"], 201)
+                       ->withHeader('Content-type', 'application/json');
+
+    return $return;
+});
+/**
+ * Atualiza os dados de um livro
+ */
+$app->put('/book/{id}', function (Request $request, Response $response) use ($app) {
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');
+//    $response->getBody()->write("Modificando o livro {$id}");
+//    return $response;
+    $return = $response->withJson(['msg'=> "Modificando o livro {$id}"], 200)
+                       ->withHeader('Content-type', 'application/json');
+    return $return;
+});
+/**
+ * Deleta o livro informado pelo ID
+ */
+$app->delete('/book/{id}', function (Request $request, Response $response) use ($app) {
+    $route = $request->getAttribute('route');
+    $id = $route->getArgument('id');
+//    $response->getBody()->write("Deletando o livro {$id}");
+//    return $response;
+    $return = $response->withJson(['msg' => "Deletando o livro {$id}"], 204)
+                       ->withHeader('Content-type', 'application/json');
+    return $return;
 });
 $app->run();
